7cf08cd8a389111a255da8d7b3ff86ea258696ae
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-07-03 19:10:12 -0400

    Refactor `TaskService` to use RxJS `BehaviorSubject`; update `TasksListComponent` for observable-based task management.

--- src/app/tasks/task.service.ts
+++ src/app/tasks/task.service.ts
@@ -2,12 +2,13 @@ import { inject, Injectable, signal } from '@angular/core';
 import { AddTask, Task, TaskStatus } from './task.model';
 import { v4 as uuidv4 } from 'uuid';
 import { LoggingService } from '../services/logging.service';
+import { BehaviorSubject } from 'rxjs';
 
 
 @Injectable({
   providedIn: 'root',
 })
-export class TaskService {
+export class TaskService3 {
   private _tasks = signal<Task[]>([]);
   private _loggingService = inject(LoggingService);
 
@@ -67,12 +68,49 @@ export class TaskService2 {
   }
 
   public updateStatus(taskId: string, newStatus: TaskStatus) {
-    this._tasks = this.tasks.map(task => {
+    this._tasks = this.tasks.map((task) => {
       if (task.id === taskId) {
         return { ...task, status: newStatus };
       }
       return task;
-    })
+    });
+
+    this._loggingService.log(`TaskId: ${taskId} - New status: ${newStatus}`);
+  }
+}
+
+
+
+// Using RxJS - Observables
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+  private _tasks = new BehaviorSubject<Task[]>([]);
+  private _loggingService = inject(LoggingService);
+  public readonly tasks$ = this._tasks.asObservable();
+
+  public add(task: AddTask) {
+    const newTask: Task = {
+      id: uuidv4(),
+      title: task.title,
+      description: task.description,
+      status: 'OPEN',
+    };
+
+    this._tasks.next([...this._tasks.value, newTask]);
+    this._loggingService.log(`Task added: ${newTask.title}`);
+  }
+
+  public updateStatus(taskId: string, newStatus: TaskStatus) {
+    this._tasks.next(
+      this._tasks.value.map(task => {
+        if (task.id === taskId) {
+          return { ...task, status: newStatus };
+        }
+        return task;
+      })
+    );
 
     this._loggingService.log(`TaskId: ${taskId} - New status: ${newStatus}`);
   }
--- src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,8 +1,16 @@
-import { Component, computed, inject, signal } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  computed,
+  DestroyRef,
+  inject,
+  OnInit,
+  signal,
+} from '@angular/core';
 
 import { TaskItemComponent } from './task-item/task-item.component';
 import { TaskService } from '../task.service';
-import { TASK_STATUS_OPTIONS, taskStatusOptionsProvider } from '../task.model';
+import { Task, TASK_STATUS_OPTIONS, taskStatusOptionsProvider } from '../task.model';
 
 @Component({
   selector: 'app-tasks-list',
@@ -12,18 +20,32 @@ import { TASK_STATUS_OPTIONS, taskStatusOptionsProvider } from '../task.model';
   imports: [TaskItemComponent],
   providers: [taskStatusOptionsProvider],
 })
-export class TasksListComponent {
+export class TasksListComponent implements OnInit {
   selectedFilter = signal<string>('ALL');
   private taskService = inject(TaskService);
   protected taskStatusOptions = inject(TASK_STATUS_OPTIONS);
+  private changeDetectorRef = inject(ChangeDetectorRef);
+  private destroyRef = inject(DestroyRef);
+  private _tasks: Task[] = [];
 
-  protected tasks = computed(() => {
+  protected get tasks() {
     if (this.selectedFilter() === 'ALL') {
-      return this.taskService.tasks();
+      return this._tasks;
     }
 
-    return this.taskService.tasks().filter((task) => task.status === this.selectedFilter());
-  });
+    return this._tasks.filter((task) => task.status === this.selectedFilter());
+  }
+
+  ngOnInit(): void {
+    const subscription = this.taskService.tasks$.subscribe((tasks) => {
+      this._tasks = tasks;
+      this.changeDetectorRef.detectChanges();
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 
   onChangeTasksFilter(filter: string) {
     this.selectedFilter.set(filter);
