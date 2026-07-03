3724f8cba3ff04912d8bffbecbda71df25f0f19d
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-07-02 23:29:16 -0400

    Inject `TASK_STATUS_OPTIONS` into `TasksListComponent` and refactor task status options into `TaskStatusOptions` using provider.

--- src/app/tasks/task.model.ts
+++ src/app/tasks/task.model.ts
@@ -1,3 +1,5 @@
+import { InjectionToken, Provider } from '@angular/core';
+
 export type TaskStatus = 'OPEN' | 'IN_PROGRESS' | 'DONE';
 
 export interface Task {
@@ -11,3 +13,26 @@ export interface AddTask {
   title: string;
   description: string;
 }
+
+export type TaskStatusOption = {
+  label: string;
+  value: TaskStatus;
+};
+
+export const TaskStatusOptions: TaskStatusOption[] = [
+  { label: 'Open', value: 'OPEN' },
+  { label: 'In Progress', value: 'IN_PROGRESS' },
+  { label: 'Completed', value: 'DONE' },
+];
+
+export const TASK_STATUS_OPTIONS = new InjectionToken<TaskStatusOption[]>('TASK_STATUS_OPTIONS',
+  // { // This is only required if I want to register the token globally, which is not the case this time.
+  //   providedIn: 'root',
+  //   factory: () => TaskStatusOptions
+  // }
+);
+
+export const taskStatusOptionsProvider: Provider = {
+  provide: TASK_STATUS_OPTIONS,
+  useValue: TaskStatusOptions
+};
--- src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/app/tasks/tasks-list/tasks-list.component.ts
@@ -2,6 +2,7 @@ import { Component, computed, inject, signal } from '@angular/core';
 
 import { TaskItemComponent } from './task-item/task-item.component';
 import { TaskService } from '../task.service';
+import { TASK_STATUS_OPTIONS, taskStatusOptionsProvider } from '../task.model';
 
 @Component({
   selector: 'app-tasks-list',
@@ -9,10 +10,12 @@ import { TaskService } from '../task.service';
   templateUrl: './tasks-list.component.html',
   styleUrl: './tasks-list.component.css',
   imports: [TaskItemComponent],
+  providers: [taskStatusOptionsProvider],
 })
 export class TasksListComponent {
   selectedFilter = signal<string>('ALL');
   private taskService = inject(TaskService);
+  protected taskStatusOptions = inject(TASK_STATUS_OPTIONS);
 
   protected tasks = computed(() => {
     if (this.selectedFilter() === 'ALL') {
