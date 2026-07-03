3db9e8af8ac62b59f446b57a36ba519fd375d5d7
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-07-02 23:46:49 -0400

    Add `TaskService2` with isolated task management and logging

--- src/app/tasks/task.service.ts
+++ src/app/tasks/task.service.ts
@@ -40,3 +40,40 @@ export class TaskService {
     this._loggingService.log(`TaskId: ${taskId} - New status: ${newStatus}`);
   }
 }
+
+
+// This approach will not work when zone.js is not used in the project
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService2 {
+  private _tasks: Task[] = [];
+  private _loggingService = inject(LoggingService);
+
+  public get tasks() {
+    return [...this._tasks];
+  }
+
+  public add(task: AddTask) {
+    const newTask: Task = {
+      id: uuidv4(),
+      title: task.title,
+      description: task.description,
+      status: 'OPEN',
+    };
+
+    this._tasks = [...this._tasks, newTask];
+    this._loggingService.log(`Task added: ${newTask.title}`);
+  }
+
+  public updateStatus(taskId: string, newStatus: TaskStatus) {
+    this._tasks = this.tasks.map(task => {
+      if (task.id === taskId) {
+        return { ...task, status: newStatus };
+      }
+      return task;
+    })
+
+    this._loggingService.log(`TaskId: ${taskId} - New status: ${newStatus}`);
+  }
+}
